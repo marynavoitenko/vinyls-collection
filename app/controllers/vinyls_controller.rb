2d78f57ce87d92c351e0266c2b4686f3f8a42011
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-13 17:06:04 -0400

    add vinyl create method

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -5,6 +5,16 @@ class VinylsController < ApplicationController
       render json: Vinyl.all
   end
 
+  def create
+    vinyl = Vinyl.new(vinyl_params)
+
+    if vinyl.save
+      render json: vinyl
+    else
+      render json: { message: 'Vinyl was not created' }, status: 422
+    end
+  end
+
   def show
       render json: @vinyl
   end
@@ -13,5 +23,9 @@ class VinylsController < ApplicationController
 
   def set_vinyl
       @vinyl = Vinyl.find(params[:id])
+  end
+
+  def vinyl_params
+    params.require(:vinyl).permit(:code, :label, :name)
   end
 end
