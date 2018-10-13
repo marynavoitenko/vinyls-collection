2379c19d18feac12bcf387fd87033d953ce72705
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-13 13:55:43 -0400

    write migrations for all models

--- db/migrate/20181013174820_create_artists.rb
+++ db/migrate/20181013174820_create_artists.rb
@@ -1,6 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
+      t.text :name
 
       t.timestamps
     end
--- db/migrate/20181013174832_create_crates.rb
+++ db/migrate/20181013174832_create_crates.rb
@@ -1,6 +1,8 @@
 class CreateCrates < ActiveRecord::Migration[5.2]
   def change
     create_table :crates do |t|
+      t.text :name
+      t.text :description
 
       t.timestamps
     end
