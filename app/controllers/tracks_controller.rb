fee2aca43eb90686fa6b47f5b73e9c50e83b75b7
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-27 20:31:51 -0400

    cleanup

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,16 +2,16 @@ class TracksController < ApplicationController
   before_action :set_track, only: [:show]
 
   def index
-      render json: Track.all
+    render json: Track.all
   end
-  
+
   def show
-      render json: @track
+    render json: @track
   end
 
   private
 
   def set_track
-      @track = Track.find(params[:id])
+    @track = Track.find(params[:id])
   end
 end
