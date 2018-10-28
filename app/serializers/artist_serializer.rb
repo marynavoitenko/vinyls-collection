50a2738a016803cd13c4cd4620a46fd237763f66
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-27 21:50:18 -0400

    Restructures artists serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,6 +1,12 @@
 class ArtistSerializer < ActiveModel::Serializer
   attributes :id, :name
 
-  has_many :tracks
   has_many :vinyls
+
+  def vinyls
+    vinyls = object.vinyls
+    vinyls.map do |vinyl|
+      vinyl.tracks.collect { |track| [id: track.id, title: track.title, artists: track.artists.map { |artist| [id: artist.id, name: artist.name] }] }
+    end
+  end
 end
