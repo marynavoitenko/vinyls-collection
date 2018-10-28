b4cb3e80c4bdbbec3ae7d8acc8505f7c747af78f
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-27 20:31:00 -0400

    Restructures vinyl serializer

--- app/serializers/vinyl_serializer.rb
+++ app/serializers/vinyl_serializer.rb
@@ -1,8 +1,11 @@
 class VinylSerializer < ActiveModel::Serializer
-  attributes :id, :code, :label, :name
+  attributes :id, :code, :name, :image_url, :release_date
 
   belongs_to :crate
   belongs_to :label
   has_many :tracks
-  has_many :artists
+
+  def tracks
+    object.tracks.collect { |track| [id: track.id, title: track.title, artists: track.artists.map { |artist| [id: artist.id, name: artist.name] }] }
+  end
 end
