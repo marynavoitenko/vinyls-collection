c052169e85f4fa515de89a37bac714c64f09fea9
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-29 22:44:57 -0400

    Adds genres associations to vinyls

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,9 +1,12 @@
 class Vinyl < ApplicationRecord
     belongs_to :crate, required: false
     belongs_to :label, required: false
-    
+
     has_many :tracks
 
     has_many :artist_vinyls
     has_many :artists, through: :artist_vinyls
+
+    has_many :genre_vinyls
+    has_many :genres, through: :genre_vinyls
 end
