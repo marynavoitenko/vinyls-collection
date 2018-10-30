9f334990724e7f8c47e07897016434c566973151
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-10-29 22:42:24 -0400

    Adds genre routes

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,17 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: [:show]
+
+  def index
+      render json: Genre.all
+  end
+
+  def show
+      render json: @genre
+  end
+
+  private
+
+  def set_genre
+      @genre = Genre.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'labels/index'
-  get 'labels/show'
   resources :artists, only: [:index, :show]
   resources :crates, only: [:index, :show]
   resources :labels, only: [:index, :show]
   resources :tracks, only: [:index, :show]
+  resources :genres, only: [:index, :show]
   resources :vinyls, only: [:index, :show, :create]
 
 end
