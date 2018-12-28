90493a3c3955736c2d9d23ad18c039b16b927501
marynavoitenko <maryna.voitenko@gmail.com>
2018-12-27 22:44:27 -0500

    Add description to label and crate serializers

--- app/serializers/crate_serializer.rb
+++ app/serializers/crate_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CrateSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :description
 
   has_many :vinyls
 end
--- app/serializers/label_serializer.rb
+++ app/serializers/label_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LabelSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :description
 
   has_many :vinyls
 end
