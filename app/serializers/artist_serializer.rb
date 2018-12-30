8fa8f76d87ffd8dc115df148ac612b629ab3bdff
marynavoitenko <maryna.voitenko@gmail.com>
2018-12-29 22:19:46 -0500

    Update artist serializer to show vinyl info instead of tracks list

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -4,11 +4,4 @@ class ArtistSerializer < ActiveModel::Serializer
   attributes :id, :name, :image_url
 
   has_many :vinyls
-
-  def vinyls
-    vinyls = object.vinyls
-    vinyls.flat_map do |vinyl|
-      vinyl.tracks.flat_map { |track| [id: track.id, title: track.title, artists: track.artists.flat_map { |artist| [id: artist.id, name: artist.name] }] }
-    end
-  end
 end
