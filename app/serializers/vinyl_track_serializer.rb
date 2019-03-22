33d9d11ce91bd6f98f7d85d11913d3cb9e2b91f0
marynavoitenko <maryna.voitenko@gmail.com>
2019-03-21 20:47:00 -0400

    ROptimize artist track serializer

--- app/serializers/vinyl_track_serializer.rb
+++ app/serializers/vinyl_track_serializer.rb
@@ -1,13 +1,5 @@
 class VinylTrackSerializer < ActiveModel::Serializer
   attributes :id, :title, :artists
 
-  # doesn't do anything - why?
-  # has_many :artists, serializer: ArtistTrackSerializer
-
-  def artists
-    ActiveModelSerializers::SerializableResource.new(
-      object.artists,
-      each_serializer: ArtistTrackSerializer
-    )
-  end
+  has_many :artists, serializer: ArtistTrackSerializer
 end
